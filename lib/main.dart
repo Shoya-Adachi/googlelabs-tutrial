a7ab8054f6257d66cabd365c3260a8a93c07be5b
ota shoya <shoya.vate@gmail.com>
2024-02-27 16:17:11 +0900

    update:中央にカードウィジェットが表示されるようにデザインを変更した

--- lib/main.dart
+++ lib/main.dart
@@ -43,16 +43,21 @@ class MyHomePage extends StatelessWidget {
     var pair = appState.current;
 
     return Scaffold(
-      body: Column(
-        children: [
-          Text('googlelabs practice'),
-          BigCard(pair: pair),
-          ElevatedButton(
-              onPressed: () {
-                appState.getNext();
-              },
-              child: Text("Next"))
-        ],
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            BigCard(pair: pair),
+            SizedBox(
+              height: 10,
+            ),
+            ElevatedButton(
+                onPressed: () {
+                  appState.getNext();
+                },
+                child: Text("Next"))
+          ],
+        ),
       ),
     );
   }
